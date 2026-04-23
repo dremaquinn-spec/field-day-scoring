e2402f68fac1b5f56e597d5f70f05dffd4bcc18c
dremaquinn-spec <dremaquinn@gmail.com>
2026-04-23 11:57:40 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -17,11 +17,11 @@ const TEACHERS_BY_GRADE = {
 
 const EVENTS_BY_GRADE = {
   "Pre-K": ["Hurdle Relay","Baton Relay","Flag Relay","Pancake Relay","Hippity Hop Relay","Hula Hoop Bean Bag","Tug of War – Girls","Tug of War – Boys"],
-  "Kinder": ["Hurdle Relay","Baton Relay","Flag Relay","Pancake Relay","Cone Flip Relay","Hippity Hop Relay","Tire Roll","Dash Relay","Relay #9","Tug of War – Girls","Tug of War – Boys"],
-  "1st": ["Hurdle Relay","Baton Relay","Flag Relay","Pancake Relay","Cone Flip Relay","Hippity Hop Relay","Tire Roll","Dash Relay","Relay #9","Tug of War – Girls","Tug of War – Boys"],
-  "2nd": ["Hurdle Relay","Baton Relay","Flag Relay","Sack Relay","Pancake Relay","3-Legged Race","Hippity Hop Relay","Tire Roll","Dash Race – Girls","Dash Race – Boys","Tug of War – Girls","Tug of War – Boys"],
-  "3rd": ["Hurdle Relay","Baton Relay","Flag Relay","Sack Relay","Jump Rope Relay","3-Legged Race","Hippity Hop Relay","Tire Roll","Dash Race – Girls","Dash Race – Boys","Tug of War – Girls","Tug of War – Boys"],
-  "4th": ["3-Legged Race","Sack Relay","Hippity Hop Relay","Tire Roll","Hurdle Race","Baton Relay","Pancake Relay","Jump Rope Relay","Flag Relay","50m Dash – Girls","50m Dash – Boys","75m Dash – Girls","75m Dash – Boys","Tug of War – Girls","Tug of War – Boys"]
+  "Kinder": ["Hurdle Relay","Hippity Hop Relay","Flag Relay","Pancake Relay","Baton Relay","Tire Roll","Dash Relay","Tug of War – Girls","Tug of War – Boys"],
+  "1st": ["Hurdle Relay","Hippity Hop Relay","Flag Relay","Pancake Relay","Cone Flip Relay","Baton Relay","Tire Roll","Dash Relay","Tug of War – Girls","Tug of War – Boys"],
+  "2nd": ["Hurdle Relay","Hippity Hop Relay","Flag Relay","Sack Relay","Pancake Relay","3-Legged Race","Baton Relay","Tire Roll","Dash Race – Girls Heat 1","Dash Race – Girls Heat 2","Dash Race – Boys Heat 1","Dash Race – Boys Heat 2","Tug of War – Girls","Tug of War – Boys"],
+  "3rd": ["Hurdle Relay","Hippity Hop Relay","Flag Relay","Sack Relay","Jump Rope Relay","3-Legged Race","Baton Relay","Tire Roll","Dash Race – Girls Heat 1","Dash Race – Girls Heat 2","Dash Race – Boys Heat 1","Dash Race – Boys Heat 2","Tug of War – Girls","Tug of War – Boys"],
+  "4th": ["Hippity Hop Relay","3-Legged Relay","Sack Relay","Tire Roll","Hurdle Race- Girls Heat 1","Hurdle Race- Girls Heat 2","Hurdle Race- Boys Heat 1","Hurdle Race- Boys Heat 2","Baton Relay","Pancake Relay","Jump Rope Relay","Flag Relay","50m Dash – Girls Heat 1","50m Dash – Girls Heat 2","50m Dash – Boys Heat 1","50m Dash – Boys Heat 2","75m Dash – Girls Heat 1","75m Dash – Girls Heat 2","75m Dash – Boys Heat 1","75m Dash – Boys Heat 2","Tug of War – Girls","Tug of War – Boys"]
 };
 
 const POINTS = { 1: 3, 2: 2, 3: 1 };
